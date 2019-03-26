84ded232eb044ca22a69567759739b0360d446c1
tranchung <tranducchung1st@gmail.com>
2019-03-26 21:39:40 +0700

    Done

---
+++ src/main/java/Variable.java
@@ -0,0 +1,16 @@
+public class Variable {
+    public static void main(String[] args) {
+        int i = 10;
+        System.out.println("i =" + i);
+        float f = 20.5f;
+        System.out.println("f=" + f);
+        double d = 20.5;
+        System.out.println("d =" + d);
+        boolean b = true;
+        System.out.println("c="+ b);
+        String s = "Ha Noi";
+        System.out.println("s=" + s);
+        char c ='a';
+        System.out.println("c= "+ c);
+    }
+}
